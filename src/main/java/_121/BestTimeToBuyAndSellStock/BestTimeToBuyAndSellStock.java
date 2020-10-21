9e4933a4174152edf955c5c71046bede3c2c33a8
Vengardd <vengardd@users.noreply.github.com>
2020-10-21 12:01:10 +0200

    121. Best Time To Buy And Sell Stock

---
+++ src/main/java/_121/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,26 @@
+package _121.BestTimeToBuyAndSellStock;
+
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                maxProfit = Math.max(maxProfit, prices[j] - prices[i]);
+            }
+        }
+        return maxProfit;
+    }
+
+    public int maxProfit2(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
