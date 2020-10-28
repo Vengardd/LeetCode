7226ba4e1965b700d82c29a6fd82a6639d9afd40
Vengardd <vengardd@users.noreply.github.com>
2020-10-28 14:54:19 +0100

    36. Valid Sudoku

---
+++ src/main/java/_36/ValidSudoku/ValidSudoku.java
@@ -0,0 +1,24 @@
+package _36.ValidSudoku;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+//        Set<String> seen = new HashSet<>();
+//        for (int i = 0; i < board.length; i++) {
+//            for (int j = 0; j < board[0].length; j++) {
+//                char actualChar = board[i][j];
+//                if (Character.isDigit(actualChar)) {
+//                    if (!seen.add(actualChar + " at row " + j) ||
+//                            !seen.add(actualChar + "at column " + i) ||)
+//                        !seen.add(actualChar + "at square " + i / 3 + " + " + j / 3) {
+//                        return false;
+//                    }
+//                }
+//            }
+//        }
+        return true;
+    }
+
+}
